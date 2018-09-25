a4d98fdf0f67070c6feeb082eca88836f1525dcf
Daniel <dzanatha@gmail.com>
2018-09-25 13:45:07 -0300

    data biding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el:'#vue-app',
   data: {
     nome: 'Zanatinha',
-    trabalho: 'Analista'
+    trabalho: 'Analista',
+    website: 'http://www.thenetninja.co.uk',
+    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Tag</a>'
   },
   methods: {
     saudacao: function(tempo) {
